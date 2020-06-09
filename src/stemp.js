4e944faa200a8b25c973f98cf9aa59642fd57e79
David Tran <davidlamt@gmail.com>
2020-06-08 21:11:33 -0700

    Remove for / if check in statement regex

--- src/stemp.js
+++ src/stemp.js
@@ -13,8 +13,8 @@ class stemp {
             '}' +
             'output += "'
           )
-          // Handle statements with <% (for|if) %>
-          .replace(/<%(.+?(for|if|}).+?)%>/g, '";$1' +
+          // Handle statements with <% %>
+          .replace(/<%(.+?)%>/g, '";$1' +
             'output += "'
           ) + 
       '}' + 
